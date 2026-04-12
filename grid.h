364e09df77e3dc9b0a83886e63265eb7bd830935
Ayman-Hachkal <ayman.hachkal@gmail.com>
2026-04-12 18:55:51 +0100

    generating fading grid lines

--- grid.h
+++ grid.h
@@ -3,11 +3,10 @@
 #include "body.h"
 #include "constants.h"
 #include "vec.h"
+#include <algorithm>
 #include <cmath>
 #include <cstdlib>
-#include <iostream>
 #include <memory>
-#include <ostream>
 #include <vector>
 #include "drawables.h"
 #include "raylib.h"
@@ -39,8 +38,8 @@ class Grid : public Drawable {
   void draw(){}
 
   void draw(const std::vector<std::shared_ptr<body>> &worldBodies, const Camera &cam) {
-    for (int x = -renderDistance; x < renderDistance; x++) {
-      for (int z = -renderDistance; z < renderDistance; z++) {
+    for (float x = -renderDistance; x < renderDistance; x++) {
+      for (float z = -renderDistance; z < renderDistance; z++) {
         float xPos = x + cam.position.x;
         float zPos = z + cam.position.z;
         point p = point(
@@ -48,24 +47,50 @@ class Grid : public Drawable {
             vec(0, 0, 0),
             vec(floor(xPos) * worldscale, 0, floor(zPos) * worldscale)
             );
+
+        point pNext = point(
+            vec(floor(xPos + 1), 0, floor(zPos)),
+            vec(0, 0, 0),
+            vec(floor(xPos + 1) * worldscale, 0, floor(zPos) * worldscale)
+            );
+
+        point pRow = point(
+            vec(floor(xPos), 0, floor(zPos + 1)),
+            vec(0, 0, 0),
+            vec(floor(xPos) * worldscale, 0, floor(zPos + 1) * worldscale)
+            );
+
         for (std::shared_ptr<body> b : worldBodies) {
           this->calculateOffsets(p, b);
+          this->calculateOffsets(pNext, b);
+          this->calculateOffsets(pRow, b);
         }
         Vector3 worldPoint = Vector3({
             static_cast<float>(p.pos.x()), 
             static_cast<float>(p.pos.y() + p.offset.y()), 
             static_cast<float>(p.pos.z())
             });
+        Vector3 worldPointNext = Vector3({
+            static_cast<float>(pNext.pos.x()), 
+            static_cast<float>(pNext.pos.y() + pNext.offset.y()), 
+            static_cast<float>(pNext.pos.z())
+            });
+        Vector3 worldPointRow = Vector3({
+            static_cast<float>(pRow.pos.x()), 
+            static_cast<float>(pRow.pos.y() + pRow.offset.y()), 
+            static_cast<float>(pRow.pos.z())
+            });
+
         //draw points 
-        //Gray scale if distance is over 80% of the render distance makes it smoother at end
+        // fade away
         Color color;
-        if (abs(x) > renderDistance * 0.8 || abs(z) > renderDistance * 0.8) {
-          color = ColorFromNormalized(Vector4({float(renderDistance)/abs(x), (float(renderDistance)/abs(x) + float(renderDistance)/abs(z))/2.0f, float(renderDistance)/abs(z), 1.0f}));
-        }
-        else {
-          color = WHITE;
-        }
+        float max = std::max(abs(x), abs(z));
+        double norm = 1.0f - ((max * max)/ (renderDistance * renderDistance));
+        color = ColorFromNormalized(Vector4(norm, norm, norm, 1.0f));
+
         DrawPoint3D(worldPoint, color);
+        DrawLine3D(worldPoint, worldPointNext, color);
+        DrawLine3D(worldPoint, worldPointRow, color);
       }
     }
   }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include "raylib.h"
 #include "scene.h"
-#include <format>
-#include <memory>
 
 int main(void) {
-  const int screenWidth = 800; 
-  const int screenHeight = 450;
+  const int screenWidth = 1280; 
+  const int screenHeight = 720;
 
   Camera camera = { { 0.0f, 10.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 };
 
@@ -17,16 +15,21 @@ int main(void) {
 
   while (!WindowShouldClose()) {
     //Update
-    UpdateCamera(&camera, CAMERA_FREE);
+    if (IsWindowFocused() && IsCursorOnScreen()){
+      UpdateCamera(&camera, CAMERA_FREE);
+    }
+    //
     //Draw
     //---------------------------------------------------
     BeginDrawing();
+    
       ClearBackground(BLACK);
       const char* cameraPosition = TextFormat("Camera Position: (%.2f, %.2f, %.2f)", camera.position.x, camera.position.y, camera.position.z);
       DrawText(cameraPosition, 10, 10, 12, WHITE);
       const char* FPS = TextFormat("FPS: %i", GetFPS());
       DrawText(FPS, 10, 30, 12, WHITE);
       blackholeScene.Draw(camera);
+
     EndDrawing();
   }
 
